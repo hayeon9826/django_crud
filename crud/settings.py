3d999bc506bc90ec2f3aa3690aedd53ca0a9e695
Hayeon <hayeon226@likelion.org>
2019-06-27 19:12:10 +0900

    배포 준비

--- crud/settings.py
+++ crud/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'akeh@x48s*ndqc$--zymr6b(1k=@^&z#ojrzm6==t+-a@ql_0s'
+# SECRET_KEY = 'akeh@x48s*ndqc$--zymr6b(1k=@^&z#ojrzm6==t+-a@ql_0s'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
